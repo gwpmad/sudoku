0c828c899f2b2f2184625daea235e1e7914adb02
gwpmad <gmaddocks1@gmail.com>
2020-04-08 17:35:17 +0100

    Add grid styles and block component

--- src/components/grid/index.tsx
+++ src/components/grid/index.tsx
@@ -1,20 +1,23 @@
 import React, { Children, FC } from 'react'
 
+import Block from './block'
+import { Container, Row } from './styles'
+
 const Grid: FC = () => {
   return (
-    <div data-cy="grid-container">
+    <Container data-cy="grid-container">
       {Children.toArray(
         [...Array(9)].map((_, rowIndex) => (
-          <div data-cy="grid-row-container" key={rowIndex}>
+          <Row data-cy="grid-row-container">
             {Children.toArray(
               [...Array(9)].map((_, colIndex) => (
-                <div data-cy="block" key={colIndex}></div>
+                <Block rowIndex={rowIndex} colIndex={colIndex} />
               ))
             )}
-          </div>
+          </Row>
         ))
       )}
-    </div>
+    </Container>
   )
 }
 
