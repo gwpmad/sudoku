987faf30adb5e7a71a7823dc8a49413a13d1e9a5
gwpmad <gmaddocks1@gmail.com>
2020-04-09 09:29:34 +0100

    Finish grid CSS

--- src/components/grid/styles/index.ts
+++ src/components/grid/styles/index.ts
@@ -7,6 +7,40 @@ export const Container = styled.div`
 `
 
 export const Row = styled.div`
-  display: flex;
-  flex-flow: row;
+  ${({ theme }) => css`
+    display: flex;
+    flex-flow: row;
+
+    /* horizontal thick lines */
+    &:nth-child(1) {
+      div {
+        border-top: solid 4px ${theme.colors.black}; /* replaces the existing border-top 1 */
+      }
+    }
+    &:nth-child(3),
+    &:nth-child(6) {
+      div {
+        border-bottom solid 3px ${theme.colors.black}; /* rows below have a border-top of 1 so this adds to them to make 4 */
+      }
+    }
+    &:nth-child(9) {
+        border-bottom solid 4px ${theme.colors.black};
+    }
+
+    /* vertical thick lines */
+    div {
+      &:nth-child(1) {
+        border-left: solid 4px ${theme.colors.black};
+      }
+      &:nth-child(3),
+      &:nth-child(6),
+      &:nth-child(9) {
+        border-right: solid 4px ${theme.colors.black};
+      }
+      &:nth-child(4),
+      &:nth-child(7) {
+        border-left: none;
+      }
+    }
+  `}
 `
